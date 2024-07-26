e7c68213bc7a8f66b2cab63d44c27ae458cdb9d8
jihad7518 <jihadmd7518@gmail.com>
2024-07-26 21:26:33 +0600

    Create News Backend implemented

--- routes/newsRoute.js
+++ routes/newsRoute.js
@@ -1,5 +1,11 @@
 const router = require('express').Router()
 const middleware = require('../middlewares/middleware')
+const newsController = require('../controllers/newsController')
+
+
+// dashboard
+
+router.post('/api/news/add', middleware.auth, newsController.add_news)
 
 
 module.exports = router
