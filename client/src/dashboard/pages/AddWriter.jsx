282739f73ec237e66d1eb77ba1b05c72e50ade04
jihad7518 <jihadmd7518@gmail.com>
2024-07-26 19:32:03 +0600

    some middleware added

--- client/src/dashboard/pages/AddWriter.jsx
+++ client/src/dashboard/pages/AddWriter.jsx
@@ -1,8 +1,46 @@
 import React, { useContext, useState } from 'react'
-import {Link} from 'react-router-dom'
-
+import { Link, useNavigate } from 'react-router-dom'
+import toast from 'react-hot-toast'
+import axios from 'axios'
+import { base_url } from '../../config/config'
+import storeContext from '../../context/storeContext'
 
 const AddWriter = () => {
+
+  const { store } = useContext(storeContext)
+  
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    password: "",
+    category: ""
+  })
+
+  const inputHandler = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const submit = async (e) => {
+    e.preventDefault()
+    try {
+      //setLoader(true)
+      const { data } = await axios.post(`${base_url}/api/news/writer/add`, state, {
+        headers: {
+          'Authorization': `Bearer ${store.token}`
+        }
+      })
+      //setLoader(false)
+      toast.success(data.message)
+      navigate('/dashboard/writers')
+    } catch (error) {
+      //setLoader(false)
+      toast.error(error.response.data.message)
+    }
+  }
+
   return (
     <div className='bg-white rounded-md'>
       <div className='flex justify-between p-4'>
@@ -11,16 +49,16 @@ const AddWriter = () => {
         hover:bg-purple-600' to='/dashboard/writers'>Writers</Link>
       </div>
       <div className='p-4'>
-        <form>
+        <form onSubmit={submit}>
           <div className='grid grid-cols-2 gap-x-8 mb-3'>
             <div className='flex flex-col gap-y-2'>
               <label className='text-md font-medium text-gray-600' htmlFor="name">Name</label>
-              <input type='text' placeholder='name' name='name' className='px-3 py-2 rounded-md 
+              <input onChange={inputHandler} value={state.name} required type='text' placeholder='name' name='name' className='px-3 py-2 rounded-md 
                   outline-0 border border-gray-300 focus:border-green-500 h-10' id='name'/>
             </div>
             <div className='flex flex-col gap-y-2'>
               <label className='text-md font-medium text-gray-600' htmlFor="category">Name</label>
-              <select name='category' id='category' className='px-3 py-2 rounded-md outline-0 border border-gray-300 focus:border-green-500 h-10' >
+              <select onChange={inputHandler} value={state.category} required name='category' id='category' className='px-3 py-2 rounded-md outline-0 border border-gray-300 focus:border-green-500 h-10' >
                 <option value="">---Select Category---</option>
                 <option value="Education">Education</option>
                 <option value="Travel">Politics</option>
@@ -34,12 +72,12 @@ const AddWriter = () => {
           <div className='grid grid-col-2 gap-x-8 mb-3'>
             <div className='flex flex-col gap-y-2'>
               <label className='text-md font-medium text-gray-600' htmlFor="email">Email</label>
-              <input type='email' placeholder='email' name='email' className='px-3 py-2 rounded-md 
+              <input onChange={inputHandler} value={state.email} required type='email' placeholder='email' name='email' className='px-3 py-2 rounded-md 
                   outline-0 border border-gray-300 focus:border-green-500 h-10' id='email'/>
             </div>
             <div className='flex flex-col gap-y-2'>
               <label className='text-md font-medium text-gray-600' htmlFor="password">Password</label>
-              <input type='password' placeholder='password' name='password' className='px-3 py-2 rounded-md 
+              <input onChange={inputHandler} value={state.password} required type='password' placeholder='password' name='password' className='px-3 py-2 rounded-md 
                   outline-0 border border-gray-300 focus:border-green-500 h-10' id='password'/>
             </div>
           </div>
