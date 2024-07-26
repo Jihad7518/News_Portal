c18d9a21d47e9fe160bdc0f8a867767433eeb4a2
jihad7518 <jihadmd7518@gmail.com>
2024-07-26 17:42:43 +0600

    Login backend authentication and db implemented

--- server.js
+++ server.js
@@ -6,20 +6,23 @@ const cors = require('cors')
 const db_connect = require('./utils/db')
 
 dotenv.config()
+
+app.use(body_parser.json())
+
 if (process.env.mode === 'production') {
     app.use(cors())
 } else {
     app.use(cors({
-        origin: "http://localhost:5173"
+        origin: ["http://localhost:5173", "http://localhost:3000"]
     }))
 }
 
 
+const port = process.env.port
+
 app.use('/', require('./routes/authRoutes'))
 app.get('/', (req, res) => res.send('Hello World!'))
 
-
-const port = process.env.port
 db_connect()
 
 app.listen(port, () => console.log(`server is running on port ${port}!`))
